0568c0a2ba1c4e6e2d50f494be66962ae5e045d5
maliknajeeb113 <maliknajeeb113@gmail.com>
2024-09-20 05:23:08 +0530

    add: footer branch init

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,25 @@
-const Footer = () =>{
+import Marquee from "react-fast-marquee";
+import logo from "../assets/images/logo.svg";
+const Footer = () => {
+  return (
+    <div className="flex flex-col w-screen items-center py-6">
+      <Marquee autoFill speed={100} pauseOnHover>
+        <div className="px-8 font-satoshi text-8xl">Follow Us</div>
+        <div className="inline-block w-[80px] h-[5px] bg-black"></div>
+      </Marquee>
 
+      <div id="footer-heads" className="flex flex-row pt-12">
+        <div id="left" className="flex flex-col">
+          <div className="flex items-center">
+            <img src={logo} className="h-6" />
+            <div className="font-satoshi font-medium px-2 text-xl">
+              Sanjeev Edupoint
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    return(<div>
-        Footer
-    </div>)
-}
-
-export default Footer
+export default Footer;
