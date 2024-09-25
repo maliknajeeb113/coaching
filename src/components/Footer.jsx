1af91e536b5527377b269292d5f9b32c0e3a8ac3
maliknajeeb113 <maliknajeeb113@gmail.com>
2024-09-25 23:57:35 +0530

    add: fixed the layout of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,13 @@ import Marquee from "react-fast-marquee";
 import logo from "../assets/images/logo.svg";
 const Footer = () => {
   return (
-    <div className="flex flex-col w-screen items-center py-6">
+    <div className="flex flex-col items-center py-6 font-satoshi">
       <Marquee autoFill speed={100} pauseOnHover>
-        <div className="px-8 font-satoshi text-8xl">Follow Us</div>
+        <div className="px-8  text-8xl">Follow Us</div>
         <div className="inline-block w-[80px] h-[5px] bg-black"></div>
       </Marquee>
 
-      <div id="footer-heads" className="flex flex-row pt-12 justify-between w-screen px-8">
+      <div id="footer-heads" className="flex flex-row pt-12 justify-between px-32 w-full">
         
         {/* left side */}
         <div id="left" className="flex flex-col w-1/4 gap-4">
@@ -18,27 +18,31 @@ const Footer = () => {
               Sanjeev Edupoint
             </div>
           </div>
-          <div className="">
+          <div className="text-xl text-gray-500">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla esse blanditiis impedit nemo aliquid. 
           </div>
         </div>
         
         {/* left side end */}
         {/* right side */}
-        <div className="flex flex-row gap-4">
+        <div className="flex flex-row gap-32">
           <ul>
+            <li className="font-medium pb-4">REACH US</li>
             <li>email id @gg.com</li>
             <li>9988775544</li>
             
           </ul>
           <ul>
+          <li className="font-medium pb-4">LEARN MORE</li>
             <li>email id @gg.com</li>
             <li>9988775544</li>
             
           </ul>
           <ul>
-            <li>email id @gg.com</li>
-            <li>9988775544</li>
+          <li className="font-medium pb-4">FOLLOW US</li>
+            <li>Instagram</li>
+            <li>Facebook</li>
+            <li>LinkedIn</li>
             
           </ul>
           
