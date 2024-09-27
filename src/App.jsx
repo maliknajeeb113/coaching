73ac100f2148b147df27adaee8686f843095d580
Sawan Kumar Bhagat <sawan8492@gmail.com>
2024-09-27 23:32:16 +0530

    NavBar Implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import './App.css'
-import Error from './pages/Error';
-import Notes from './pages/Notes';
-import Body from './pages/Body';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import "./App.css";
+import Error from "./pages/Error";
+import Notes from "./pages/Notes";
+import Body from "./pages/Body";
+import Navbar from "./components/Navbar";
+import Contact from "./components/Contact";
+import Testimonial from "./components/Testimonial";
+import Footer from "./components/Footer";
+import { Element } from "react-scroll";
 
 import { createBrowserRouter,Outlet,Link } from "react-router-dom";
 import { useState } from 'react'
@@ -11,32 +14,41 @@ import { useState } from 'react'
 function App() {
 
   return (
-    <>
-    <Navbar/>
-    <Outlet/>
-    <Footer/>
-    </>
-  )
+    <div className="flex flex-col justify-center items-center gap-y-32">
+      <Navbar />
+      <Outlet />
+      <Element name="testimonials">
+        <section id="testimonials">
+          <Testimonial />
+        </section>
+      </Element>
+      <Element name="contact">
+        <section id="contact">
+          <Contact />
+        </section>
+      </Element>
+
+      <Footer />
+    </div>
+  );
 }
 
 export const appRouter = createBrowserRouter([
-{
-
-  path: "/",
-  element: <App/>,
-  errorElement: <Error/>,
-  children: [
-    {
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children: [
+      {
       path: "/",
       element: <Body/>
-    },
-    {
-      path: "/notes",
-      element: <Notes/>
-    }
-  ]
-
-}
+      },
+      {
+        path: "/notes",
+        element: <Notes/>
+      }
+    ]
+  }
 
 ])
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,85 @@
-const Navbar = () =>{
+import React, { useState } from "react";
+import { Link } from "react-scroll";
+import DragHandleRoundedIcon from "@mui/icons-material/DragHandleRounded";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 
+const NavBar = () => {
+  let navItems = [
+    { name: "Home", id: "home" },
+    { name: "Notes", id: "notes" },
+    { name: "Testimonials", id: "testimonials" },
+    { name: "Contact", id: "contact" },
+  ];
 
-    return(<div className="flex w-full bg-violet-300">
-        Navbar here
-    </div>)
-}
+  const [isOpen, setIsOpen] = useState(false);
+  const [activePage, setActivePage] = useState("home");
 
-export default Navbar
+  const handleOnClick = (id) => {
+    setIsOpen(!isOpen);
+    setActivePage(id);
+    console.log(activePage);
+  };
+
+  return (
+    <div>
+      <div className="fixed left-0 px-[20px] xs:px-[30px] w-full h-20 flex justify-between items-center bg-white backdrop-blur-[10px] z-20 lg:top-[-490px]">
+        <div>
+          <span>Coaching</span>
+        </div>
+        <div onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? (
+            <CloseRoundedIcon
+              className="bg-[#1f2426] text-white text-4xl p-1 rounded-[50%] hidden"
+              fontSize=""
+            />
+          ) : (
+            <DragHandleRoundedIcon
+              className="bg-[#1f2426] text-white text-4xl p-1 rounded-[50%] hidden"
+              fontSize=""
+            />
+          )}
+        </div>
+      </div>
+
+      <ul
+        className={`lg:flex gap-[30px] lg:rounded-[30px] mx-[20px] xs:mx-[30px] lg:mx-0 p-[20px_30px] lg:p-[8px_13px] fixed lg:top-[33px] w-[calc(100%-40px)] xs:w-[calc(100%-60px)] lg:w-auto left-0 lg:left-1/2 bg-[#1f2426] lg:bg-[#1f2426e6] backdrop-blur-[10px] text-white lg:transform lg:translate-x-[-50%] z-10 transition-all duration-500 ease-in lg:transition-none ${
+          isOpen ? "top-20" : "top-[-490px]"
+        }`}
+      >
+        {navItems.map((navItem) => (
+          <li
+            key={navItem.id}
+            className={`tracking-[.03em] leading-[30px] uppercase rounded-[40px] py-[5px] lg:py-0 lg:px-[10px] text-[13px] font-normal no-underline lg:hover:bg-white lg:hover:text-black hover:cursor-pointer duration-200 flex relative ${
+              activePage === navItem.id ? "lg:bg-white lg:text-black" : ""
+            }`}
+          >
+            <Link
+              to={navItem.id}
+              smooth
+              spy
+              offset={-100}
+              duration={500}
+              onSetActive={(id) => setActivePage(id)}
+              onClick={() => handleOnClick(navItem.id)}
+              name={navItem.id}
+            >
+              {navItem.id == "home" ? (
+                <>
+                  <span className="hidden lg:block">
+                    <HomeRoundedIcon />
+                  </span>
+                  <span className="lg:hidden">{navItem.name}</span>
+                </>
+              ) : (
+                navItem.name
+              )}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
