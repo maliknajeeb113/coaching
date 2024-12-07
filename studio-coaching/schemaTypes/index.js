1f77afbd230d6af90614e63a473620cd7c02c687
Sawan Kumar Bhagat <sawan8492@gmail.com>
2024-12-08 00:44:39 +0530

    Removed backend

--- studio-coaching/schemaTypes/index.js
+++
@@ -1,5 +0,0 @@
-import { classType } from './classType'
-import { noteType } from './noteType'
-import { subjectType } from './subjectType'
-
-export const schemaTypes = [classType, subjectType ,noteType]
